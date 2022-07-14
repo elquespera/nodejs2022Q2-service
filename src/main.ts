9d31abf2dc251d4da12376dc3eda744c9571ff1a
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-07-14 18:15:14 +0200

    feat: add swagger at '/doc' route and edit api.yaml according to task description

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse as parseYaml } from 'yaml';
+import { resolve as resolvePath, dirname } from 'path';
 
 import 'dotenv/config';
 import 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { readFile } from 'fs/promises';
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const apiDocFile = await readFile(
+    resolvePath('..', dirname(__dirname), 'doc', 'api.yaml'),
+    'utf-8',
+  );
+  const apiDoc = parseYaml(apiDocFile);
+  SwaggerModule.setup('doc', app, apiDoc);
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
