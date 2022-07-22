0283663b6d7a2500433daadc8d06ac0211ac4fb4
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-07-22 15:11:35 +0200

    feat: implement common entity with id

---
+++ src/modules/common.entity.ts
@@ -0,0 +1,9 @@
+import { PrimaryGeneratedColumn } from "typeorm";
+import { v4 as uuidv4 } from 'uuid';
+
+export abstract class CommonEntity {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuidv4();
+  
+}
