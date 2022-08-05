5ca86f4832b2a9fd54fab7bd1f837f00c1d64f8f
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-08-05 16:36:21 +0200

    feat: implement token expiry constants from .env

--- src/modules/auth/auth.constants.ts
+++ src/modules/auth/auth.constants.ts
@@ -1,7 +1,7 @@
 export const jwtConstants = {
   access_key: process.env.JWT_SECRET_KEY || 'jwt-secret',
-  access_expiry: 300,
+  access_expiry: process.env.TOKEN_EXPIRE_TIME || '1h',
 
   refresh_key: process.env.JWT_SECRET_REFRESH_KEY || 'jwt-refresh-secret',
-  refresh_expiry: 600,
+  refresh_expiry: process.env.TOKEN_REFRESH_EXPIRE_TIME || '24h',
 };
