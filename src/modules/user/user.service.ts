377405985d7c7cbd5ff3a18506c11dc1ea697dad
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-08-05 10:03:33 +0200

    feat: implement bcrypt for hashing passwords

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto, UserDto, UpdatePasswordDto } from './user.dto';
 import { forbidden, notFound } from '../utils';
@@ -12,6 +13,10 @@ export class UserService {
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
   ) {}
+
+  private hash(data: string) {
+    return bcrypt.hash(data, 10);
+  }
 
   async findUser(id: string): Promise<UserEntity> {
     const user = await this.userRepository.findOne({ where: { id: id } });
@@ -26,7 +31,7 @@ export class UserService {
 
   async match(dto: CreateUserDto): Promise<UserEntity> {
     const users = await this.userRepository.find();
-    return users.find(({ login, password }) => login === dto.login && password === dto.password);
+    return users.find(async ({ login, password }) => login === dto.login && await bcrypt.compare(password, dto.password));
   }
 
   async findAll() {
@@ -35,18 +40,19 @@ export class UserService {
   }
 
   async create(dto: CreateUserDto): Promise<UserDto> {
-    const createdUser = this.userRepository.create(dto);
+    const passwordHash = await this.hash(dto.password);
+    const createdUser = this.userRepository.create({ login: dto.login, password: passwordHash });
     const savedUser = await this.userRepository.save(createdUser); 
     return savedUser.format();
   }
 
   async update(userId: string, updateDto: UpdatePasswordDto): Promise<UserDto> {
     const updatedUser = await this.findUser(userId);
-
-    if (updatedUser.password !== updateDto.oldPassword)
+    const doPasswordsMatch = await bcrypt.compare(updateDto.oldPassword, updatedUser.password);
+    if (!doPasswordsMatch)
       forbidden(`The old password does not match`);
 
-    updatedUser.password = updateDto.newPassword;
+    updatedUser.password = await this.hash(updateDto.newPassword);
     updatedUser.version += 1;
     updatedUser.updatedAt = Date.now().toString();
 
